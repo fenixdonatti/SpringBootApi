c41ae5e039af0a0cc7daa8b61da1e4e48093bec7
fenixdonatti <fenixdonatti@gmail.com>
2026-02-02 14:59:30 -0300

    commit inicial

---
+++ src/test/java/io/github/SpringBootApi/ProdutosApi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.SpringBootApi.ProdutosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
